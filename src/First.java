acd7189f50d9265bc3c6043a5e29c3f47a1e5964
olena.yarmolenko <yarmolenko.alena@gmail.com>
2018-09-10 19:18:52 +0300

    initial commit

---
+++ src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main (String[] args) {
+        String out = "Hello World!";
+        int i=1;
+        int a=2+2;
+        System.out.println(out + i + a);
+    }
+}
