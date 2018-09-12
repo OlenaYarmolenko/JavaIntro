6e48faf842412599fc0e8a089fbb7fed4c2bc40a
olena.yarmolenko <yarmolenko.alena@gmail.com>
2018-09-12 19:25:17 +0300

    add second programm

---
+++ src/Second/Person.java
@@ -0,0 +1,41 @@
+package Second;
+
+public class Person {
+    String firstName;
+    String secondName;
+    int age;
+    private static int count = 0;
+
+    public Person(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+        count++;
+    }
+
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        count++;
+    }
+    void doSomething (){
+        println(count + " makes smth.");
+    }
+
+    private static void println(String x) {
+        System.out.println(x);
+    }
+
+    static void doSomethingElse (){
+        println(count + " makes smth else");
+    }
+    static{
+        println("class was initialized");
+    }
+    {
+        println(count + " person was created");
+    }
+    private static void Println(String out) {
+        println(out);
+    }
+}
